b1fdb12699fb641f9487dc61a707de20c18eb20d
Chris Vasquez <chrvasq@outlook.com>
2020-02-28 22:34:21 -0800

    Add remaining tests for User-model

--- tests/user-model.test.js
+++ tests/user-model.test.js
@@ -1,30 +1,22 @@
 const db = require("../data/dbConfig");
-const Users = require("../api/users/users-model");
 const bcrypt = require("bcryptjs");
+const {
+  findUsers,
+  addUser,
+  findUserById,
+  findUserByUsername,
+  deleteUser,
+  updateUser
+} = require("../api/users/users-model");
 
 beforeEach(async () => {
   await db("users").truncate();
 });
 
 describe("Users model", () => {
-  describe("findUsers", () => {
-    beforeEach(async () => {
-      await Users.addUser({
-        username: "Chris",
-        password: bcrypt.hashSync("pass", 10)
-      });
-    });
-
-    it("should return an array of user objects", async () => {
-      const users = await Users.findUsers();
-
-      expect(users).toEqual([{ id: 1, username: "Chris" }]);
-    });
-  });
-
   describe("addUser", () => {
     it("should add a new user and return the newly created user", async () => {
-      const user = await Users.addUser({
+      const user = await addUser({
         username: "Chris",
         password: bcrypt.hashSync("pass", 10)
       });
@@ -33,34 +25,83 @@ describe("Users model", () => {
     });
   });
 
-  describe("findUserById", () => {
-    beforeEach(async () => {
-      await Users.addUser({
+  describe("findUsers", () => {
+    it("should return an array of user objects", async () => {
+      await addUser({
         username: "Chris",
         password: bcrypt.hashSync("pass", 10)
       });
-    });
 
+      const users = await findUsers();
+
+      expect(users).toEqual([{ id: 1, username: "Chris" }]);
+    });
+  });
+
+  describe("findUserById", () => {
     it("should return the user for the id passed in", async () => {
-      const user = await Users.findUserById(1);
+      await addUser({
+        username: "Chris",
+        password: bcrypt.hashSync("pass", 10)
+      });
+
+      const user = await findUserById(1);
 
       expect(user).toEqual({ id: 1, username: "Chris" });
     });
   });
 
   describe("deleteUser", () => {
-    beforeEach(async () => {
-      await Users.addUser({
+    it("should delete the user for the id passed in", async () => {
+      await addUser({
         username: "Chris",
         password: bcrypt.hashSync("pass", 10)
       });
-    });
 
-    it("should delete the user for the id passed in", async () => {
-      await Users.deleteUser(1);
-      const users = await Users.findUserById(1);
+      await deleteUser(1);
+
+      const users = await findUserById(1);
 
       expect(users).toBe(undefined);
+    });
+  });
+
+  describe("findUserByUsername", () => {
+    it("should return user by username", async () => {
+      await addUser({
+        username: "Chris",
+        password: bcrypt.hashSync("pass", 10)
+      });
+
+      const user = await findUserByUsername("Chris");
+
+      expect(user.username).toBe("Chris");
+    });
+  });
+
+  describe("updateUser", () => {
+    it("should update username and password", async () => {
+      await addUser({
+        username: "Chris",
+        password: bcrypt.hashSync("pass", 10)
+      });
+
+      const user = await findUserByUsername("Chris");
+
+      expect(user.username).toBe("Chris");
+      expect(bcrypt.compareSync("pass", user.password)).toBe(true);
+
+      const changes = {
+        username: "Mike",
+        password: bcrypt.hashSync("newpass", 10)
+      };
+
+      await updateUser(1, changes);
+
+      const updatedUser = await findUserByUsername("Mike");
+
+      expect(updatedUser.username).toEqual("Mike");
+      expect(bcrypt.compareSync("newpass", updatedUser.password)).toBe(true);
     });
   });
 });
