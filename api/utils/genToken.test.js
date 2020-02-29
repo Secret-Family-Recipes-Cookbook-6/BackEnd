62e2c27293e45f1aa80bad85117837f7e7bd36f3
Chris Vasquez <chrvasq@outlook.com>
2020-02-28 23:52:53 -0800

    Add genToken util

---
+++ api/utils/genToken.test.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const genToken = require("./genToken");
+const jwt = require("jsonwebtoken");
+
+describe("getToken", () => {
+  it("returns a JWT token", () => {
+    user = {
+      id: 1,
+      username: "Chris"
+    };
+    const token = genToken(user);
+
+    const tokenDecoded = jwt.decode(token);
+
+    expect(tokenDecoded.sub).toBe(1);
+    expect(tokenDecoded.username).toBe("Chris");
+  });
+});
