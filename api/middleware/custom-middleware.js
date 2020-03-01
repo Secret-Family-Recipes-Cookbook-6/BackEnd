c536ec888fb15c4e6980b962d81fca4ed18e191f
Chris Vasquez <chrvasq@outlook.com>
2020-02-29 22:49:13 -0800

    Add validateRecipe middleware and tests

--- api/middleware/custom-middleware.js
+++ api/middleware/custom-middleware.js
@@ -15,7 +15,28 @@ const validateUser = (req, res, next) => {
   }
 };
 
-const validateRecipe = (req, res, next) => {};
+const validateRecipe = (req, res, next) => {
+  const { title, source, ingredients, instructions, category } = req.body;
+
+  if (Object.keys(req.body).length !== 0) {
+    if (title && source && ingredients && instructions && category) {
+      req.validRecipe = { ...req.body };
+      next();
+    } else if (!title) {
+      res.status(400).json({ message: "Title required." });
+    } else if (!source) {
+      res.status(400).json({ message: "Source required." });
+    } else if (!ingredients) {
+      res.status(400).json({ message: "Ingredients required." });
+    } else if (!instructions) {
+      res.status(400).json({ message: "Instructions required." });
+    } else if (!category) {
+      res.status(400).json({ message: "Category required." });
+    }
+  } else {
+    res.status(400).json({ message: "Missing required information." });
+  }
+};
 
 const auth = (req, res, next) => {};
 
--- tests/custom-middleware.test.js
+++ tests/custom-middleware.test.js
@@ -14,11 +14,9 @@ const mockResponse = () => {
   return res;
 };
 
-const next = jest.fn();
-
 describe("custom-middleware", () => {
   describe("validateUser", () => {
-    it("should 400 if username and password are missing from body", async () => {
+    it("should 400 if body is empty", async () => {
       const req = mockRequest({});
       const res = mockResponse();
 
@@ -57,12 +55,141 @@ describe("custom-middleware", () => {
     it("should call next if username and password are in body", async () => {
       const req = mockRequest({ username: "Chris", password: "pass" });
       const res = mockResponse();
+      const next = jest.fn();
 
       await validateUser(req, res, next);
 
       expect(req.validUser).toStrictEqual({
         username: "Chris",
         password: "pass"
+      });
+      expect(next).toHaveBeenCalled();
+    });
+  });
+
+  describe("validateRecipe", () => {
+    it("should 400 if body is empty", async () => {
+      const req = mockRequest({});
+      const res = mockResponse();
+
+      await validateRecipe(req, res);
+
+      expect(res.status).toHaveBeenCalledWith(400);
+      expect(res.json).toHaveBeenCalledWith({
+        message: "Missing required information."
+      });
+    });
+
+    it("should 400 if title is missing from the body", async () => {
+      const req = mockRequest({
+        source: "Grandma Edna",
+        ingredients: "Beef, cheese, lettuce, tomato",
+        instructions:
+          "Grill the burgers while adding cheese towards the end. Place on bun with condiments and serve.",
+        category: "Lunch"
+      });
+      const res = mockResponse();
+
+      await validateRecipe(req, res);
+
+      expect(res.status).toHaveBeenCalledWith(400);
+      expect(res.json).toHaveBeenCalledWith({
+        message: "Title required."
+      });
+    });
+
+    it("should 400 if source is missing from the body", async () => {
+      const req = mockRequest({
+        title: "Hamburger",
+        ingredients: "Beef, cheese, lettuce, tomato",
+        instructions:
+          "Grill the burgers while adding cheese towards the end. Place on bun with condiments and serve.",
+        category: "Lunch"
+      });
+      const res = mockResponse();
+
+      await validateRecipe(req, res);
+
+      expect(res.status).toHaveBeenCalledWith(400);
+      expect(res.json).toHaveBeenCalledWith({
+        message: "Source required."
+      });
+    });
+
+    it("should 400 if ingredients is missing from the body", async () => {
+      const req = mockRequest({
+        title: "Hamburger",
+        source: "Grandma Edna",
+        instructions:
+          "Grill the burgers while adding cheese towards the end. Place on bun with condiments and serve.",
+        category: "Lunch"
+      });
+      const res = mockResponse();
+
+      await validateRecipe(req, res);
+
+      expect(res.status).toHaveBeenCalledWith(400);
+      expect(res.json).toHaveBeenCalledWith({
+        message: "Ingredients required."
+      });
+    });
+
+    it("should 400 if instructions is missing from the body", async () => {
+      const req = mockRequest({
+        title: "Hamburger",
+        source: "Grandma Edna",
+        ingredients: "Beef, cheese, lettuce, tomato",
+        category: "Lunch"
+      });
+      const res = mockResponse();
+
+      await validateRecipe(req, res);
+
+      expect(res.status).toHaveBeenCalledWith(400);
+      expect(res.json).toHaveBeenCalledWith({
+        message: "Instructions required."
+      });
+    });
+
+    it("should 400 if category is missing from the body", async () => {
+      const req = mockRequest({
+        title: "Hamburger",
+        source: "Grandma Edna",
+        ingredients: "Beef, cheese, lettuce, tomato",
+        instructions:
+          "Grill the burgers while adding cheese towards the end. Place on bun with condiments and serve."
+      });
+      const res = mockResponse();
+
+      await validateRecipe(req, res);
+
+      expect(res.status).toHaveBeenCalledWith(400);
+      expect(res.json).toHaveBeenCalledWith({
+        message: "Category required."
+      });
+    });
+
+    it("should call next if all required properties are in the body", async () => {
+      const req = mockRequest({
+        title: "Hamburger",
+        source: "Grandma Edna",
+        ingredients: "Beef, cheese, lettuce, tomato",
+        instructions:
+          "Grill the burgers while adding cheese towards the end. Place on bun with condiments and serve.",
+        category: "Lunch"
+      });
+      const res = mockResponse();
+      const next = jest.fn();
+
+      await validateRecipe(req, res, next);
+
+      expect(req.validRecipe).toStrictEqual({
+        title: "Hamburger",
+        source: "Grandma Edna",
+        ingredients: "Beef, cheese, lettuce, tomato",
+        instructions:
+          "Grill the burgers while adding cheese towards the end. Place on bun with condiments and serve.",
+        category: "Lunch"
       });
       expect(next).toHaveBeenCalled();
     });
