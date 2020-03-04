98c959f2bbae83751087fa7fae2c7638c8bf57bf
Chris Vasquez <chrvasq@outlook.com>
2020-03-04 01:36:44 -0800

    Add recipes and user seeds

--- data/seeds/01-users.js
+++ data/seeds/01-users.js
@@ -1,9 +1,21 @@
 const bcrypt = require("bcryptjs");
 
 exports.seed = function(knex) {
-  return knex("users").insert({
-    username: "Chris",
-    email: "test1@email.com",
-    password: bcrypt.hashSync("pass", 10)
-  });
+  return knex("users").insert([
+    {
+      username: "Chris",
+      email: "test1@email.com",
+      password: bcrypt.hashSync("pass", 10)
+    },
+    {
+      username: "Mike",
+      email: "test2@email.com",
+      password: bcrypt.hashSync("pass", 10)
+    },
+    {
+      username: "Charlie",
+      email: "test3@email.com",
+      password: bcrypt.hashSync("pass", 10)
+    }
+  ]);
 };
