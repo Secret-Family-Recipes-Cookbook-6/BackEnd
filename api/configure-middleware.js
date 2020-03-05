1a066767a8f414cb126c1256555f0a9801baeefc
Chris Vasquez <chrvasq@outlook.com>
2020-03-05 01:32:17 -0800

    Remove unecessary imports

--- api/configure-middleware.js
+++ api/configure-middleware.js
@@ -2,12 +2,10 @@ const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
 const apiRouter = require("./routes/api-router");
-const { resolve } = require("path");
 
 module.exports = server => {
   server.use(helmet()),
     server.use(cors()),
     server.use(express.json()),
-    server.use("/api", apiRouter),
-    server.use(express.static(resolve(__dirname, "/public")));
+    server.use("/api", apiRouter);
 };
--- api/server.js
+++ api/server.js
@@ -1,12 +1,7 @@
 const express = require("express");
 const configureMiddleware = require("./configure-middleware");
-const { resolve } = require("path");
 
 const server = express();
 configureMiddleware(server);
-
-// server.get("/*", (req, res) =>
-//   res.sendFile(resolve(__dirname, "../public/index.html"))
-// );
 
 module.exports = server;
